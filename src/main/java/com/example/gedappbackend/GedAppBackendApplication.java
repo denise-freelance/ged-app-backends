01f8648a485562240b9f9fce3f752d00ee9c4113
denise-freelance <denisekamssue@gmail.com>
2026-05-13 11:45:04 +0100

    First commit.

---
+++ src/main/java/com/example/gedappbackend/GedAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gedappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GedAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gedappbackend/GedAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gedappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
